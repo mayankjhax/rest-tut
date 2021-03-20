aa3d1227b610b5a0415202e795a44423a65cef35
Mayank Jha <mjha2532005@gmail.com>
2021-03-20 18:04:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv/config')
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const cors = require('cors')
+
+// Middleware: also use for authentication
+app.use(cors()) // this gives access to this to every domain, closing the cross origin resource sharing
+app.use(express.urlencoded())
+app.use(express.json())
+
+
+// import routes
+const postsRoute = require('./routes/posts')
+app.use('/posts', postsRoute)
+
+app.get('/', (req, res) => {
+    res.send('We are on home')
+})
+
+// Connect to db
+mongoose
+    .connect(
+        process.env.MONGODB_URL,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+    .then(() => {
+        console.log("Connected to mongodb cloud")
+    })
+    .catch(err => console.log(err))
+
+app.listen(8000)
+
+// fetch('http://localhost:8000/posts')
+// .then(result=>{
+//   return result.json()
+// })
+// .then(data=>{
+//   console.log(data)
+// }) Use this code in javascript for getting and using the api
---
+++ routes/posts.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const router = express.Router()
+const Post = require('../models/Post')
+
+// This one gets back all the posts
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find()
+        res.json(posts)
+    } catch (err) {
+        res.json({ message: err })
+    }
+})
+
+// Specific post
+router.get('/:postID', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postID)
+        res.json(post)
+    } catch (err) {
+        res.json({ message: err })
+    }
+})
+
+// Submits a post
+router.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    })
+
+    try {
+        const savedPost = await post.save()
+        res.json(savedPost)
+    } catch (err) {
+        res.json({ message: err })
+    }
+})
+
+// Delete post
+router.delete('/:postID', async (req, res) => {
+    try {
+        const removedPost = await Post.remove({ _id: req.params.postID })
+        res.json(removedPost)
+    } catch (err) {
+        res.json({ message: err })
+    }
+})
+
+// Update a post
+router.patch('/:postID', async (req, res) => {
+    try {
+        const updatedPost = await Post.updateOne(
+            { _id: req.params.postID },
+            { $set: { title: req.body.title } },
+            { $set: { description: req.body.description } }
+        )
+        res.json(updatedPost)
+    } catch (err) {
+        res.json({ message: err })
+    }
+})
+
+module.exports = router
