e19ac325474a8ba4201439edf469d89f37c208a4
Mayank Jha <mjha2532005@gmail.com>
2021-03-21 19:40:36 +0530

    add rest.http and done some changes for efficient code

--- app.js
+++ app.js
@@ -1,37 +1,34 @@
-require('dotenv/config')
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const cors = require('cors')
+require("dotenv/config");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
 
 // Middleware: also use for authentication
-app.use(cors()) // this gives access to this to every domain, closing the cross origin resource sharing
-app.use(express.urlencoded())
-app.use(express.json())
-
+app.use(cors()); // this gives access to this to every domain, closing the cross origin resource sharing
+app.use(express.urlencoded());
+app.use(express.json());
 
 // import routes
-const postsRoute = require('./routes/posts')
-app.use('/posts', postsRoute)
+const postsRoute = require("./routes/posts");
+app.use("/posts", postsRoute);
 
-app.get('/', (req, res) => {
-    res.send('We are on home')
-})
+app.get("/", (req, res) => {
+  res.send("We are on home");
+});
 
 // Connect to db
 mongoose
-    .connect(
-        process.env.MONGODB_URL,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-    .then(() => {
-        console.log("Connected to mongodb cloud")
-    })
-    .catch(err => console.log(err))
-
-app.listen(8000)
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(8000, () => {
+      console.log("Server listening on port 8000");
+    });
+  })
+  .catch((err) => console.log(err));
 
 // fetch('http://localhost:8000/posts')
 // .then(result=>{
